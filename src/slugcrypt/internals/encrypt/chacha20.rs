0008cce9b1e804b7e32550fe7b3642e8d41c4400
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-01-20 09:43:32 -0500

    Update chacha20.rs

--- src/slugcrypt/internals/encrypt/chacha20.rs
+++ src/slugcrypt/internals/encrypt/chacha20.rs
@@ -1,10 +1,15 @@
+use std::str::Utf8Error;
+use base58::{FromBase58,ToBase58,FromBase58Error};
 use chacha20poly1305::{
     aead::{Aead, KeyInit, OsRng}, XNonce, AeadCore, Error, Key, Nonce, XChaCha20Poly1305 // Cipher, key, and nonce types
 };
 
+use std::string::FromUtf8Error;
 use rand::CryptoRng;
 use rand::RngCore;
 use zeroize::{Zeroize,ZeroizeOnDrop};
+
+use subtle_encoding::hex;
 
 #[derive(Zeroize,ZeroizeOnDrop)]
 pub struct EncryptionKey {
@@ -13,7 +18,7 @@ pub struct EncryptionKey {
 
 #[derive(Zeroize,ZeroizeOnDrop)]
 pub struct EncryptionNonce {
-    nonce: Vec<u8>
+    nonce: [u8;24]
 }
 
 #[derive(Zeroize,ZeroizeOnDrop)]
@@ -25,11 +30,35 @@ impl EncryptionCipherText {
     pub fn as_bytes(&self) -> &[u8] {
         &self.ciphertext
     }
+    pub fn bs58(&self) -> String {
+        self.ciphertext.to_base58()
+    }
+    pub fn from_bs58(s: &str) -> Result<Self,FromBase58Error> {
+        let bs58 = s.from_base58()?;
+
+        return Ok(Self {
+            ciphertext: bs58
+        })
+    }
 }
 
 impl EncryptionNonce {
     pub fn as_bytes(&self) -> &[u8] {
         &self.nonce
+    }
+    pub fn to_hex(&self) -> Result<String,FromUtf8Error> {
+        let bytes = subtle_encoding::hex::encode_upper(self.nonce);
+        let hex_key = String::from_utf8(bytes)?;
+        return Ok(hex_key)
+    }
+    pub fn from_hex(hex_str: &str) -> Result<Self,subtle_encoding::Error> {
+        let nonce = subtle_encoding::hex::decode_upper(hex_str)?;
+
+        let nonce_array: [u8;24] = nonce.try_into().unwrap();
+
+        return Ok(Self {
+            nonce: nonce_array,
+        })
     }
 }
 
@@ -44,14 +73,25 @@ impl EncryptionKey {
             key: key,
         }
     }
-    pub fn to_hex(&self) -> Vec<u8> {
-        subtle_encoding::hex::encode_upper(self.key)
+    pub fn to_hex(&self) -> Result<String,FromUtf8Error> {
+        let bytes = subtle_encoding::hex::encode_upper(self.key);
+        let hex_key = String::from_utf8(bytes)?;
+        return Ok(hex_key)
     }
     pub fn as_array(&self) -> [u8;32] {
         self.key
     }
     pub fn as_bytes(&self) -> &[u8] {
         return &self.key
+    }
+    pub fn from_hex(hex_str: &str) -> Result<Self,subtle_encoding::Error> {
+        let key = subtle_encoding::hex::decode_upper(hex_str)?;
+
+        let key_array: [u8;32] = key.try_into().unwrap();
+
+        return Ok(Self {
+            key: key_array,
+        })
     }
 }
 
@@ -64,7 +104,11 @@ impl SlugEncrypt {
 
         //let mut nonce_array: [u8;24] = [0u8;24];
 
+        // Nonce_Vec
         let nonce_vec = nonce.as_slice().to_vec();
+
+        // Nonce Array (passed to Self)
+        let nonce_array: [u8;24] = nonce_vec.try_into().unwrap();
         
 
         let ciphertext = cipher.encrypt(&nonce, data.as_ref())?;
@@ -74,7 +118,7 @@ impl SlugEncrypt {
                 ciphertext: ciphertext
             },
             EncryptionNonce {
-                nonce: nonce_vec
+                nonce: nonce_array
             })
         )
     }
