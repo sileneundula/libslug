5cbc8f61e0304f4c8daee21bd8930d29af2f202b
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-02-01 12:06:59 -0500

    Update ml_kem.rs

--- src/slugcrypt/internals/encryption/ml_kem.rs
+++ src/slugcrypt/internals/encryption/ml_kem.rs
@@ -1,4 +1,5 @@
 use ml_kem::kem::DecapsulationKey;
+use ml_kem::kem::EncapsulationKey;
 use ml_kem::EncodedSizeUser;
 use ml_kem::MlKem1024;
 use ml_kem::KemCore;
@@ -8,6 +9,8 @@ use ml_kem::ArraySize;
 use ml_kem::MlKem1024Params;
 use rand::rngs::OsRng;
 use hybrid_array::Array;
+
+use crate::errors::SlugErrors;
 
 pub struct MLKEMPublicKey {
     pub public_key: [u8;1568],
@@ -47,6 +50,32 @@ impl MLKEMSecretKey {
     pub fn to_usable_type(&self) -> DecapsulationKey<MlKem1024Params> {
         DecapsulationKey::from_bytes(Array::from_slice(&self.secret_key))
     }
+    pub fn public_key(&self) -> Result<MLKEMPublicKey,SlugErrors> {
+        let bytes = self.to_usable_type().encapsulation_key().as_bytes();
+        MLKEMPublicKey::from_bytes(&bytes)
+    }
+}
+
+impl MLKEMPublicKey {
+    pub fn from_array_bytes(bytes: [u8;1568]) -> Self {
+        return Self {
+            public_key: bytes 
+        }
+    }
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self,SlugErrors> {
+        let mut byte_array: [u8;1568] = [0u8;1568];
+        
+        if bytes.len() == 1568 {
+            byte_array.copy_from_slice(&bytes);
+            return Ok(Self {public_key: byte_array})
+        }
+        else {
+            return Err(SlugErrors::InvalidLengthFromBytes)
+        }
+    }
+    pub fn to_usable_type(&self) -> EncapsulationKey<MlKem1024Params> {
+        EncapsulationKey::from_bytes(Array::from_slice(&self.public_key))
+    }
 }
 
 #[test]
@@ -55,4 +84,5 @@ fn main() {
 
     println!("Public Key Len: {}",pk.public_key.len());
     println!("Secret Key Len: {}",sk.secret_key.len());
+
 }
