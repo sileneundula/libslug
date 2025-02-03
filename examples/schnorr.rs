6d11a2bf284ae29f9bec0f0fed07ccc28e395ba8
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-02-03 15:04:59 -0500

    Update schnorr.rs

--- examples/schnorr.rs
+++ examples/schnorr.rs
@@ -2,5 +2,15 @@ use libslug::slugcrypt::internals::signature::schnorr::{SchnorrPublicKey,Schnorr
 
 fn main() {
     let sk = SchnorrSecretKey::generate();
+    let pk = sk.public_key().unwrap();
     let signature = sk.sign_with_context("This is a message", "SlugCrypt").unwrap();
+    let output = pk.verify_with_context("This is a message", "SlugCrypt", signature);
+
+    if output.is_ok() {
+        println!("Valid Signature");
+    }
+    else {
+        println!("Invalid Signature");
+    }
+
 }
