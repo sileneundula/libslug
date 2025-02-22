8b54c270dede679289ce36e647d40e5f87f7f7a1
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-02-22 12:58:58 -0500

    Libslug API

--- src/slugcrypt/api/mod.rs
+++ src/slugcrypt/api/mod.rs
@@ -1,6 +1,10 @@
 pub struct SlugCrypt;
 
+pub struct SlugAsyCrypt;
+
 use crate::slugcrypt::internals::encrypt::chacha20::*;
+use crate::slugcrypt::internals::encrypt::aes256::{EncryptAES256, DecryptAES256};
+use crate::slugcrypt::internals::encrypt::aes256;
 
 impl SlugCrypt {
     pub fn encrypt<T: AsRef<[u8]>>(key: EncryptionKey, data: T) -> Result<(EncryptionCipherText,EncryptionNonce),chacha20poly1305::aead::Error> {
@@ -11,5 +15,17 @@ impl SlugCrypt {
         let x = SlugEncrypt::decrypt(key, nonce, data)?;
         return Ok(x)
     }
+    pub fn encrypt_aes256<T: AsRef<[u8]>>(key: aes256::EncryptionKey, data: T) -> Result<(aes256::AESCipherText,aes256::EncryptionNonce),aes_gcm::Error> {
+        let x: (aes256::AESCipherText, aes256::EncryptionNonce) = EncryptAES256::encrypt(key, data.as_ref())?;
+        return Ok(x)
+    }
+    pub fn decrypt_aes256(key: aes256::EncryptionKey, nonce: aes256::EncryptionNonce, data: aes256::AESCipherText) -> Result<Vec<u8>,aes_gcm::Error> {
+        let x = DecryptAES256::decrypt(key, nonce, data)?;
+        return Ok(x)
+    }
+}
+
+impl SlugAsyCrypt {
+
 }
 
