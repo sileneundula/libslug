ecd12913322c76329f85606fb43ae560bd7ee9e2
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-02-25 09:12:01 -0500

    Create sha3.rs

---
+++ examples/sha3.rs
@@ -0,0 +1,11 @@
+use libslug::slugcrypt::internals::digest::sha3::Sha3Hasher;
+use libslug::slugcrypt::internals::digest::digest::SlugDigest;
+
+fn main() {
+    let mut hasher = Sha3Hasher::new(384);
+    let data = "hello world";
+    let bytes = hasher.digest(data.as_bytes());
+
+    let result = SlugDigest::from_bytes(&bytes).unwrap();
+    println!("SHA3 (384): {}", result.digest());
+}
