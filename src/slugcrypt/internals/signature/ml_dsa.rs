d9c5e97fc7427a203050a6656489982821f58309
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-04-19 09:39:16 -0400

    Update ml_dsa.rs

--- src/slugcrypt/internals/signature/ml_dsa.rs
+++ src/slugcrypt/internals/signature/ml_dsa.rs
@@ -18,20 +18,20 @@ pub struct SlugMLDSA;
 
 impl SlugMLDSA {
     pub fn generate() -> MLDSAKeypair {
-        let mut rng = OsRng::default();
-        let kp = ml_dsa::MlDsa65::key_gen(&mut rng);
+        let mut rng: OsRng = OsRng::default();
+        let kp: ml_dsa::KeyPair<ml_dsa::MlDsa65> = ml_dsa::MlDsa65::key_gen(&mut rng);
 
         println!("MLDSA Keypair Generated");
         println!("Secret Key: {}", kp.signing_key().encode().len());
         println!("Public Key: {}", kp.verifying_key().encode().len());
 
-        let mut pk_output = [0u8; 1952];
-        let mut sk_output = [0u8; 4032];
+        let mut pk_output: [u8; 1952] = [0u8; 1952];
+        let mut sk_output: [u8; 4032] = [0u8; 4032];
         pk_output.copy_from_slice(kp.verifying_key().encode().as_ref());
         sk_output.copy_from_slice(kp.signing_key().encode().as_ref());
 
-        let public_key = MLDSAPublicKey { pk: pk_output };
-        let secret_key = MLDSASecretKey { sk: sk_output };
+        let public_key: MLDSAPublicKey = MLDSAPublicKey { pk: pk_output };
+        let secret_key: MLDSASecretKey = MLDSASecretKey { sk: sk_output };
 
         return MLDSAKeypair {
             public_key,
