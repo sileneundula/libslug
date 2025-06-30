acdc3343b67615674a550f123744b3eed297d091
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-06-30 08:08:08 -0400

    Change Files

--- src/slugcrypt/internals/signature/ml_dsa.rs
+++ src/slugcrypt/internals/signature/ml_dsa.rs
@@ -6,22 +6,33 @@ use serde_big_array::BigArray;
 use zeroize::{Zeroize, ZeroizeOnDrop};
 use crate::errors::SlugErrors;
 
+//use hybrid_array::ArrayN;
+use hybrid_array_new::ArrayN;
+
+use rand::RngCore;
+use rand::CryptoRng;
+
+pub const MLDSA3_PUBLIC_KEY_SIZE: usize = 1952;
+pub const MLDSA3_SECRET_KEY_SIZE: usize = 4032;
+pub const MLDSA3_SIGNATURE_SIZE: usize = 3309;
+
+
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Zeroize, ZeroizeOnDrop)]
 pub struct MLDSA3PublicKey {
     #[serde(with = "BigArray")]
-    pub pk: [u8; 1952],
+    pub pk: [u8; MLDSA3_PUBLIC_KEY_SIZE],
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Zeroize, ZeroizeOnDrop)]
 pub struct MLDSA3SecretKey {
     #[serde(with = "BigArray")]
-    pub sk: [u8; 4032],
+    pub sk: [u8; MLDSA3_SECRET_KEY_SIZE],
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Zeroize, ZeroizeOnDrop)]
 pub struct MLDSA3Signature {
     #[serde(with = "BigArray")]
-    pub signature: [u8; 1952],
+    pub signature: [u8; MLDSA3_SIGNATURE_SIZE],
 }
 
 pub struct MLDSA3Keypair {
@@ -35,10 +46,6 @@ impl SlugMLDSA3 {
     pub fn generate() -> MLDSA3Keypair {
         let mut rng: OsRng = OsRng::default();
         let kp: ml_dsa::KeyPair<ml_dsa::MlDsa65> = ml_dsa::MlDsa65::key_gen(&mut rng);
-
-        println!("MLDSA Keypair Generated");
-        println!("Secret Key: {}", kp.signing_key().encode().len());
-        println!("Public Key: {}", kp.verifying_key().encode().len());
 
         let mut pk_output: [u8; 1952] = [0u8; 1952];
         let mut sk_output: [u8; 4032] = [0u8; 4032];
@@ -62,6 +69,30 @@ impl MLDSA3Keypair {
     pub fn secret_key(&self) -> &MLDSA3SecretKey {
         &self.secret_key
     }
+    pub fn sign<T: AsRef<[u8]>>(&self, message: T, ctx: T) -> Result<MLDSA3Signature, ml_dsa::Error> {
+        self.secret_key.sign(message, ctx)
+    }
+}
+
+impl MLDSA3PublicKey {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, SlugErrors> {
+        let mut pk_array: [u8; 1952] = [0u8; 1952];
+
+        if bytes.len() == 1952 {
+            pk_array.copy_from_slice(bytes);
+            Ok(Self { pk: pk_array })
+        } else {
+            Err(SlugErrors::InvalidLengthFromBytes)
+        }
+    }
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.pk
+    }
+    pub fn to_usable_type(&self) -> ml_dsa::VerifyingKey<ml_dsa::MlDsa65> {
+        let hybrid = hybrid_array_new::ArrayN::<u8, 1952>::from_slice(&self.pk);
+        let usable: ml_dsa::VerifyingKey<ml_dsa::MlDsa65> = ml_dsa::VerifyingKey::decode(hybrid);
+        return usable;
+    }
 }
 
 impl MLDSA3SecretKey {
@@ -78,19 +109,48 @@ impl MLDSA3SecretKey {
     pub fn as_bytes(&self) -> &[u8] {
         &self.sk
     }
-    /*
-    pub fn to_usable_type(&self) -> Result<ml_dsa::SigningKey<ml_dsa::MlDsa65>, ml_dsa::Error> {
-        //ml_dsa::SigningKey::decode(&self.sk)
+    pub fn to_usable_type(&self) -> ml_dsa::SigningKey<ml_dsa::MlDsa65> {
+        let hybrid = hybrid_array_new::ArrayN::<u8, 4032>::from_slice(&self.sk);
+        let usable: ml_dsa::SigningKey<ml_dsa::MlDsa65> = ml_dsa::SigningKey::decode(hybrid);
+        return usable;
     }
-    
-    pub fn sign<T: AsRef<[u8]>>(&self, message: T) -> Result<ml_dsa::Signature<ml_dsa::MlDsa65>, ml_dsa::Error> {
-        let sk = self.to_usable_type()?;
-        Ok(sk.sign(message.as_ref()))
+    pub fn sign<T: AsRef<[u8]>>(&self, message: T, ctx: T) -> Result<MLDSA3Signature, ml_dsa::Error> {
+        let sk = self.to_usable_type();
+        let mut rng = OsRng::default();
+        let d = sk.sign_randomized(message.as_ref(), ctx.as_ref(), &mut rng)?;
+        
+        let sig = MLDSA3Signature::from_bytes(d.encode().as_ref()).unwrap();
+        Ok(sig)
+
     }
-    */
+}
+
+impl MLDSA3Signature {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, SlugErrors> {
+        let mut sig_array: [u8; 1424] = [0u8; 1424];
+
+        if bytes.len() == 1424 {
+            sig_array.copy_from_slice(bytes);
+            Ok(Self { signature: sig_array })
+        } 
+        else {
+            Err(SlugErrors::InvalidLengthFromBytes)
+        }
+    }
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.signature
+    }
+    pub fn to_usable_type(&self) -> ml_dsa::Signature<ml_dsa::MlDsa65> {
+        let hybrid = hybrid_array_new::ArrayN::<u8, 3309>::from_slice(&self.signature);
+        let usable: ml_dsa::Signature<ml_dsa::MlDsa65> = ml_dsa::Signature::decode(hybrid).unwrap();
+        return usable;
+    }
 }
 
 #[test]
 fn gen() {
-    
+    let keypair = SlugMLDSA3::generate();
+    keypair.sign("Hello, Word!", "Context").unwrap();
+
+
 }
