88361609636d288bb1b2716bce97016167a344b1
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-07-02 09:07:49 -0400

    Add SlugTrait

--- src/slugcrypt/slugusage/slugtrait.rs
+++ src/slugcrypt/slugusage/slugtrait.rs
@@ -7,9 +7,23 @@ pub trait SlugMessageTrait {
 }
 
 pub trait SlugEncoding {
+    // Bytes
+    fn as_bytes(&self) -> &[u8];
+    fn from_bytes(bytes: &[u8]);
+    
+    // Hex
     fn to_hex(&self, bytes: &[u8]);
     fn from_hex(&self, s_hex: &str);
 
+    // Base32
+    fn to_base32(&self, bytes: &[u8]);
+    fn from_base32(&self, bytes: &[u8]);
+
+    // Base58
     fn to_base58(&self, bytes: &[u8]);
     fn from_base58(&self, s_bs58: &str);
+
+    // Base64
+    fn to_base64(&self, bytes: &[u8]);
+    fn from_base64(&self, s_bs64: &str);
 }
