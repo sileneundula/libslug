b7163d035d9f85ca2d5aad2b51d81ec761e1770e
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-08-01 10:02:40 -0400

    Add documentation

--- src/slugcrypt/internals/signature/schnorr.rs
+++ src/slugcrypt/internals/signature/schnorr.rs
@@ -1,7 +1,8 @@
-use std::string::FromUtf8Error;
+//! # Schnorr Signatures
+//! 
+//! Schnorr signatures over Ristresto.
 
-/// # Schnorr Signatures
-/// 
+use std::string::FromUtf8Error;
 
 use schnorrkel::*;
 
@@ -15,28 +16,44 @@ use subtle_encoding::hex;
 use schnorrkel::{Keypair, vrf::{VRFInOut, VRFProof, VRFPreOut, VRFSigningTranscript, Malleable}};
 use schnorrkel::context::SigningContext;
 
-
+/// SLUGCRYPT CONTEXT
 pub const SLUGCRYPT_CONTEXT: &str = "SlugCrypt";
 
+
+/// # Schnorr: Public Key
+/// 
+/// The public key is 32-bytes in size.
 #[derive(Zeroize,ZeroizeOnDrop,Serialize,Deserialize, Clone)]
 pub struct SchnorrPublicKey([u8;32]);
 
+/// # Schnorr: Secret Key
+/// 
+/// The secret key is 64-bytes in size.
 #[derive(Zeroize,ZeroizeOnDrop,Serialize,Deserialize, Clone)]
 pub struct SchnorrSecretKey(#[serde(with = "BigArray")][u8;64]);
 
+/// # Schnorr: Signature
+/// 
+/// The signature is 64-bytes in size.
 #[derive(Zeroize,ZeroizeOnDrop,Serialize,Deserialize, Clone)]
 pub struct SchnorrSignature(#[serde(with = "BigArray")][u8;64]);
 
+/// # Schnorr: VRF Proof
+/// 
+/// Verifiable Random Function Proof
 #[derive(Zeroize,ZeroizeOnDrop,Serialize,Deserialize, Clone)]
 pub struct SchnorrVRFProof(#[serde(with = "BigArray")]pub [u8;64]);
 
+/// # Schnorr: IO
 #[derive(Zeroize,ZeroizeOnDrop,Serialize,Deserialize, Debug, Clone)]
 pub struct SchnorrIO(pub [u8;32]);
 
+/// # Schnorr: Preout
 #[derive(Zeroize,ZeroizeOnDrop,Serialize,Deserialize, Debug, Clone)]
 pub struct SchnorrPreout(pub [u8;32]);
 
 impl SchnorrIO {
+    /// From Bytes (32-bytes)
     pub fn from_bytes(bytes: &[u8]) -> Result<Self,SlugErrors> {
         let mut io_array: [u8;32] = [0u8;32];
 
@@ -51,6 +68,7 @@ impl SchnorrIO {
 }
 
 impl SchnorrVRFProof {
+    /// From Bytes (64-bytes)
     pub fn from_bytes(bytes: &[u8]) -> Result<Self,SlugErrors> {
         let mut proof_array: [u8;64] = [0u8;64];
 
@@ -65,6 +83,7 @@ impl SchnorrVRFProof {
 }
 
 impl SchnorrPreout {
+    /// From Bytes (32-bytes)
     pub fn from_bytes(bytes: &[u8]) -> Result<Self,SlugErrors> {
         let mut preout_array: [u8;32] = [0u8;32];
 
@@ -79,17 +98,23 @@ impl SchnorrPreout {
 }
 
 impl SchnorrSecretKey {
+    /// # Schnorr: Generation
+    /// 
+    /// Generates a secret key
     pub fn generate() -> Self {
         let sk = schnorrkel::SecretKey::generate();
         let sk_bytes = sk.to_bytes();
         return Self(sk_bytes)
     }
+    /// as_bytes()
     pub fn as_bytes(&self) -> &[u8] {
         return &self.0
     }
+    /// to_bytes (64-bytes)
     pub fn to_bytes(&self) -> [u8;64] {
         self.0
     }
+    /// From Bytes (64-bytes)
     pub fn from_bytes(bytes: &[u8]) -> Result<Self,SlugErrors> {
         let mut sk_array: [u8;64] = [0u8;64];
         
@@ -101,18 +126,22 @@ impl SchnorrSecretKey {
             return Err(SlugErrors::InvalidLengthFromBytes)
         }
     }
+    /// To Usable Type
     pub fn to_usable_type(&self) -> Result<schnorrkel::SecretKey,schnorrkel::SignatureError> {
         schnorrkel::SecretKey::from_bytes(&self.0)
     }
+    /// Sign with Context
     pub fn sign_with_context<T: AsRef<[u8]>>(&self, msg: T, context: T) -> Result<SchnorrSignature, SignatureError>  {
         let sk = self.to_usable_type()?;
         let pk = self.to_usable_type()?.to_public();
         
         Ok(SchnorrSignature::from_bytes(&sk.sign_simple_doublecheck(context.as_ref(), msg.as_ref(), &pk)?.to_bytes()).unwrap())
     }
+    /// Sign with Slugcrypt
     pub fn sign_with_slugcrypt<T: AsRef<[u8]>>(&self, msg: T) -> Result<SchnorrSignature, SignatureError> {
         self.sign_with_context(msg.as_ref(), SLUGCRYPT_CONTEXT.as_bytes())
     }
+    /// Verifiable Random Function
     pub fn vrf<T: AsRef<[u8]>>(&self, msg: T, signing_context: T) -> (SchnorrIO,SchnorrVRFProof,SchnorrPreout) {
         let keypair = Keypair::from(self.to_usable_type().unwrap());
         let ctx = SigningContext::new(signing_context.as_ref());
@@ -126,33 +155,41 @@ impl SchnorrSecretKey {
 
         return (vrfio,vrfproof,vrfpreout)
     }
+    /// To Public Key Type
     pub fn to_public_key_type(&self) -> Result<schnorrkel::PublicKey,schnorrkel::SignatureError> {
         let sk = self.to_usable_type()?;
         Ok(sk.to_public())
     }
+    /// Into Public Key
     pub fn public_key(&self) -> Result<SchnorrPublicKey,schnorrkel::SignatureError> {
         let pk = self.to_public_key_type()?;
         Ok(SchnorrPublicKey::from_bytes(&pk.to_bytes()).unwrap())
     }
+    /// To Hex String (Upper)
     pub fn to_hex_string(&self) -> Result<String,FromUtf8Error> {
         let hex_bytes = hex::encode_upper(self.0);
         Ok(String::from_utf8(hex_bytes)?)
     }
+    /// From Hex String (Upper)
     pub fn from_hex_string<T: AsRef<str>>(hex_str: T) -> Result<Vec<u8>, subtle_encoding::Error> {
         hex::decode_upper(hex_str.as_ref().as_bytes())
     }
 }
 
 impl SchnorrPublicKey {
+    /// as_bytes()
     pub fn as_bytes(&self) -> &[u8] {
         &self.0
     }
+    /// to_bytes()
     pub fn to_bytes(&self) -> [u8;32] {
         self.0
     }
+    /// to usable type
     pub fn to_usable_type(&self) -> Result<schnorrkel::PublicKey,SignatureError> {
         schnorrkel::PublicKey::from_bytes(&self.0)
     }
+    /// verify vrf
     pub fn verify_vrf<T: AsRef<[u8]>>(&self, vrf_preout: SchnorrPreout, vrf_io: SchnorrIO, vrf_proof: SchnorrVRFProof, transcript: T, msg: T) -> Result<(VRFInOut, vrf::VRFProofBatchable), SignatureError>  {
         let pk = self.to_usable_type()?;
 
@@ -162,6 +199,7 @@ impl SchnorrPublicKey {
         
         pk.vrf_verify(transcript.bytes(msg.as_ref()), &preout, &vrf_proof)
     }
+    /// from bytes
     pub fn from_bytes(bytes: &[u8]) -> Result<Self,SlugErrors> {
         let mut pk_array: [u8;32] = [0u8;32];
 
@@ -173,6 +211,7 @@ impl SchnorrPublicKey {
             Err(SlugErrors::InvalidLengthFromBytes)
         }
     }
+    /// Verify with context
     pub fn verify_with_context<T: AsRef<[u8]>>(&self, msg: T, context: T, signature: SchnorrSignature) -> Result<(), SignatureError> {
         let validation = self.to_usable_type().unwrap().verify_simple(context.as_ref(), msg.as_ref(), &signature.to_usable_type().unwrap());
 
@@ -188,29 +227,36 @@ impl SchnorrPublicKey {
         self.verify_with_context(msg, "SlugCrypt".as_bytes().to_owned(), signature)
     }
     */
+    /// to base58
     pub fn to_base58_string(&self) -> String {
         self.0.to_base58()
     }
+    /// from base58
     pub fn from_base58_string<T: AsRef<str>>(bs58_str: T) -> Result<Vec<u8>,FromBase58Error> {
         let bytes = bs58_str.as_ref().from_base58()?;
         Ok(bytes)
     }
+    /// to hex string (upper)
     pub fn to_hex_string(&self) -> Result<String,FromUtf8Error> {
         let hex_bytes = hex::encode_upper(self.0);
         Ok(String::from_utf8(hex_bytes)?)
     }
+    /// from hex string (upper)
     pub fn from_hex_string<T: AsRef<str>>(hex_str: T) -> Result<Vec<u8>, subtle_encoding::Error> {
         hex::decode_upper(hex_str.as_ref().as_bytes())
     }
 }
 
 impl SchnorrSignature {
+    /// as_bytes()
     pub fn as_bytes(&self) -> &[u8] {
         &self.0
     }
+    /// to_bytes()
     pub fn to_bytes(&self) -> [u8;64] {
         self.0
     }
+    /// from bytes (64-bytes)
     pub fn from_bytes(bytes: &[u8]) -> Result<Self,SlugErrors> {
         let mut sig_array: [u8;64] = [0u8;64];
 
@@ -222,20 +268,25 @@ impl SchnorrSignature {
             Err(SlugErrors::InvalidLengthFromBytes)
         }
     }
+    /// to usable type
     pub fn to_usable_type(&self) -> Result<Signature, SignatureError> {
         schnorrkel::Signature::from_bytes(&self.0)
     }
+    /// to base58 string
     pub fn to_base58_string(&self) -> String {
         self.0.to_base58()
     }
+    /// from base58 string
     pub fn from_base58_string<T: AsRef<str>>(bs58_str: T) -> Result<Vec<u8>,FromBase58Error> {
         let bytes = bs58_str.as_ref().from_base58()?;
         Ok(bytes)
     }
+    /// to hex string
     pub fn to_hex_string(&self) -> Result<String,FromUtf8Error> {
         let hex_bytes = hex::encode_upper(self.0);
         Ok(String::from_utf8(hex_bytes)?)
     }
+    /// from hex string
     pub fn from_hex_string<T: AsRef<str>>(hex_str: T) -> Result<Vec<u8>, subtle_encoding::Error> {
         hex::decode_upper(hex_str.as_ref().as_bytes())
     }
--- src/slugcrypt/internals/signature/sphincs_plus.rs
+++ src/slugcrypt/internals/signature/sphincs_plus.rs
@@ -1,3 +1,23 @@
+//! # SPHINCS+ (SHAKE256) (Smaller Signatures) (Level 5, 254 bit security)
+//! 
+//! SPHINCS+ is a stateless hash based digital signature algorithm that relies on the hardness of finding collisions in hash functions.  It has the following properties:
+//! 
+//! ## Key Size:
+//! 
+//! - Public Key: 64 bytes
+//! 
+//! - Secret Key: 128 bytes
+//! 
+//! - Signature: 29792 bytes
+//! 
+//! ## Security
+//! 
+//! This implementation uses pqcrypto sphincs+
+//! 
+//! ## TODO
+//! 
+//! - Remove Message Struct
+
 use std::primitive;
 
 use pqcrypto_sphincsplus::sphincsshake256ssimple::{PublicKey as PublicKeySphincs, SecretKey as SecretKeySphincs, DetachedSignature as DetachedSignatureSphincs};
@@ -13,18 +33,27 @@ use serde_big_array::BigArray;
 use subtle_encoding::hex;
 use base58::{FromBase58, FromBase58Error, ToBase58};
 
+/// # SPHINCS: Public Key
+/// 
+/// Size of 64 bytes
 #[derive(Debug,Zeroize,ZeroizeOnDrop,Serialize,Deserialize, Clone)]
 pub struct SPHINCSPublicKey {
     #[serde(with = "BigArray")]
     pk: [u8;64]
 }
 
+/// # SPHINCS: Secret Key
+/// 
+/// Size of 128 bytes
 #[derive(Debug,Zeroize,ZeroizeOnDrop,Serialize,Deserialize, Clone)]
 pub struct SPHINCSSecretKey {
     #[serde(with = "BigArray")]
     sk: [u8;128]
 }
 
+/// # SPHINCS Signature
+/// 
+/// Size of 29_792 bytes
 #[derive(Debug,Zeroize,ZeroizeOnDrop,Serialize,Deserialize,Clone)]
 pub struct SPHINCSSignature {
     #[serde(with = "BigArray")]
@@ -32,6 +61,7 @@ pub struct SPHINCSSignature {
 }
 
 impl SPHINCSSecretKey {
+    /// Generate keypair using operating system randomness
     pub fn generate() -> (SPHINCSPublicKey,SPHINCSSecretKey) {
         let keypair = keypair();
 
@@ -46,6 +76,7 @@ impl SPHINCSSecretKey {
 }
 
 impl SPHINCSPublicKey {
+    /// From Bytes (64-bytes)
     pub fn from_bytes(bytes: &[u8]) -> std::result::Result<SPHINCSPublicKey,SlugErrors> {
         let mut pk_array: [u8;64] = [0u8;64];
 
@@ -62,9 +93,11 @@ impl SPHINCSPublicKey {
         
 
     }
+    /// as bytes
     pub fn as_bytes(&self) -> &[u8] {
         &self.pk
     }
+    /// to usable type
     pub fn to_usable_type(&self) -> std::result::Result<PublicKeySphincs,SlugErrors> {
         let pk = PublicKeySphincs::from_bytes(self.as_bytes());
 
@@ -75,22 +108,29 @@ impl SPHINCSPublicKey {
             return Ok(pk.unwrap())
         }
     }
-    pub fn verify(&self, msg: Message, sig: SPHINCSSignature) -> std::result::Result<bool,VerificationError> {
-        let verification = verify_detached_signature(&sig.to_usable_type().unwrap(), msg.as_bytes(), &self.to_usable_type().unwrap())?;
+    /// # Verify
+    /// 
+    /// Verifies msg and SPHINCSSignature
+    pub fn verify<T: AsRef<[u8]>>(&self, msg: T, sig: SPHINCSSignature) -> std::result::Result<bool,VerificationError> {
+        let verification = verify_detached_signature(&sig.to_usable_type().unwrap(), msg.as_ref(), &self.to_usable_type().unwrap())?;
 
         return Ok(true)
 
     }
+    /// To Hex String  
     pub fn to_hex_string(&self) -> std::result::Result<String, std::string::FromUtf8Error> {
         let bytes = hex::encode_upper(self.pk);
         Ok(String::from_utf8(bytes)?)
     }
+    /// From Hex String
     pub fn from_hex_string<T: AsRef<str>>(hex_str: T) -> std::result::Result<Vec<u8>,subtle_encoding::Error> {
         Ok(hex::decode_upper(hex_str.as_ref().as_bytes())?)
     }
+    /// To Base58 String
     pub fn to_base58_string(&self) -> String {
         self.pk.to_base58()
     }
+    /// From Base58 String
     pub fn from_base58_string<T: AsRef<str>>(bs58_str: T) -> std::result::Result<Vec<u8>, FromBase58Error> {
         let bytes = bs58_str.as_ref().from_base58()?;
         Ok(bytes)
@@ -98,6 +138,7 @@ impl SPHINCSPublicKey {
 }
 
 impl SPHINCSSecretKey {
+    /// From Bytes (128-bytes)
     pub fn from_bytes(bytes: &[u8]) -> std::result::Result<SPHINCSSecretKey, SlugErrors> {
         let mut sk_array: [u8;128] = [0u8;128];
 
@@ -112,11 +153,15 @@ impl SPHINCSSecretKey {
             return Err(SlugErrors::InvalidLengthFromBytes)
         }
     }
+    /// as bytes
     pub fn as_bytes(&self) -> &[u8] {
         &self.sk
     }
-    pub fn sign(&self, msg: Message) -> std::result::Result<SPHINCSSignature,SlugErrors> {
-        let bytes = detached_sign(msg.as_bytes(), &self.to_usable_type().unwrap());
+    /// # Sign
+    /// 
+    /// Signs a message
+    pub fn sign<T: AsRef<[u8]>>(&self, msg: T) -> std::result::Result<SPHINCSSignature,SlugErrors> {
+        let bytes = detached_sign(msg.as_ref(), &self.to_usable_type().unwrap());
 
         let signature = SPHINCSSignature::from_bytes(bytes.as_bytes())?;
 
@@ -132,22 +177,27 @@ impl SPHINCSSecretKey {
             return Ok(sk.unwrap())
         }
     }
+    /// To Hex String (Upper)
     pub fn to_hex_string(&self) -> std::result::Result<String, std::string::FromUtf8Error> {
         let bytes = hex::encode_upper(self.sk);
         Ok(String::from_utf8(bytes)?)
     }
+    /// From Hex String (Upper)
     pub fn from_hex_string<T: AsRef<str>>(hex_str: T) -> std::result::Result<Vec<u8>,subtle_encoding::Error> {
         Ok(hex::decode_upper(hex_str.as_ref().as_bytes())?)
     }
 }
 
 impl SPHINCSSignature {
+    /// To Bytes
     pub fn to_bytes(&self) -> Vec<u8> {
         self.signature.to_vec()
     }
+    /// As Bytes
     pub fn as_bytes(&self) -> &[u8] {
         &self.as_bytes()
     }
+    /// From Bytes (29972 bytes)
     pub fn from_bytes(bytes: &[u8]) -> std::result::Result<SPHINCSSignature,SlugErrors> {
         let mut signature_array: [u8;29_792] = [0u8;29_792];
 
@@ -160,21 +210,26 @@ impl SPHINCSSignature {
             return Err(SlugErrors::InvalidLengthFromBytes)
         }
     }
+    /// to usable type
     fn to_usable_type(&self) -> std::result::Result<DetachedSignatureSphincs,Error> {
         let signature = DetachedSignatureSphincs::from_bytes(&self.to_bytes())?;
 
         return Ok(signature)
     }
+    /// to hex string (upper)
     pub fn to_hex_string(&self) -> std::result::Result<String, std::string::FromUtf8Error> {
         let bytes = hex::encode_upper(self.signature);
         Ok(String::from_utf8(bytes)?)
     }
+    /// from hex string (upper)
     pub fn from_hex_string<T: AsRef<str>>(hex_str: T) -> std::result::Result<Vec<u8>,subtle_encoding::Error> {
         Ok(hex::decode_upper(hex_str.as_ref().as_bytes())?)
     }
+    /// to base58 string
     pub fn to_base58_string(&self) -> String {
         self.signature.to_base58()
     }
+    /// from base58 string
     pub fn from_base58_string<T: AsRef<str>>(bs58_str: T) -> std::result::Result<Vec<u8>, FromBase58Error> {
         let bytes = bs58_str.as_ref().from_base58()?;
         Ok(bytes)
@@ -190,7 +245,7 @@ fn keypair_ls() {
     println!("Public Key Length: {}",keypair.0.as_bytes().len());
     println!("Secret Key Length: {}",keypair.1.as_bytes().len());
 
-    let signature = keypair.1.sign(message).unwrap();
+    let signature = keypair.1.sign("Hello World. This is using SPHINCS+.").unwrap();
 
     let length: usize = signature.to_bytes().len();
 
