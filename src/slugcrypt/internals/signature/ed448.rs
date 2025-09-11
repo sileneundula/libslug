98941a1c9402240aeec4bdc33cc0713d3dc4a0dc
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-09-11 11:21:28 -0400

    Update ed448.rs

--- src/slugcrypt/internals/signature/ed448.rs
+++ src/slugcrypt/internals/signature/ed448.rs
@@ -1,7 +1,7 @@
 use ed448_goldilocks_plus::{EdwardsPoint, CompressedEdwardsY, Scalar, elliptic_curve::hash2curve::ExpandMsgXof, sha3::Shake256};
 use rand::rngs::OsRng;
 
-pub struct ED448PublicKey;
+pub struct ED448PublicKey(pub [u8;57]);
 
 pub struct ED448SecretKey(pub [u8;56]);
 
@@ -25,7 +25,13 @@ impl ED448SecretKey {
     pub fn to_byte_array(&self) -> [u8;56] {
         return self.0
     }
-    pub fn public_key(&self) -> {
-        EdwardsPoint::GENERATOR * &self.0
+    pub fn public_key(&self) -> ED448PublicKey {
+        let pk = EdwardsPoint::GENERATOR * &self.to_usable_type();
+        let pk_compressed = pk.compress();
+        let pk_bytes = pk_compressed.as_bytes();
+        return ED448PublicKey(pk_bytes.to_owned())
+    }
+    pub fn to_usable_type(&self) -> Scalar {
+        return Scalar::from_bytes(&self.0)
     }
 }
