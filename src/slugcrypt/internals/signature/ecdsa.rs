2107342bd3c1db73c435a05c2499cce129e14f5e
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-09-14 09:49:53 -0400

    Update ecdsa.rs

--- src/slugcrypt/internals/signature/ecdsa.rs
+++ src/slugcrypt/internals/signature/ecdsa.rs
@@ -24,18 +24,42 @@ impl ECDSASecretKey {
 
         ECDSASecretKey(bytes)
     }
-    pub fn sign<T: AsRef<[u8]>>(&self, msg: T) -> Result<() {
-        let signature = self.to_usable_type().sign_recoverable(msg.as_ref())?;
+    pub fn sign<T: AsRef<[u8]>>(&self, msg: T) -> Result<(ecdsa::Signature<Secp256k1>, ecdsa::RecoveryId), ecdsa::Error> {
+        let signature: (ecdsa::Signature<Secp256k1>, ecdsa::RecoveryId) = self.to_usable_type().sign_recoverable(msg.as_ref())?;
+        return Ok(signature)
     }
-    pub fn to_usable_type(&self) -> SigningKey<Secp256k1> {
-        SigningKey::from_bytes(&self.0)
+    pub fn to_usable_type(&self) -> Result<SigningKey,ecdsa::Error> {
+        let key: ecdsa::SigningKey<Secp256k1> = SigningKey::from_slice(&self.0)?;
+        return Ok(key)
     }
     pub fn to_usable_type_pk(&self) -> VerifyingKey<Secp256k1> {
-        self.to_usable_type().verifying_key()
+        self.to_usable_type().verifying_key()?;
     }
     pub fn public_key(&self) -> ECDSAPublicKey {
+        let mut output_bytes: [u8;32] = [0u8;32];
         let bytes = self.to_usable_type_pk().to_sec1_bytes();
-        ECDSAPublicKey(bytes)
+        let final_bytes = bytes.to_vec();
+
+        if final_bytes.len() == 32 {
+            output_bytes.copy_from_slice(final_bytes);
+        }
+        ECDSAPublicKey(output_bytes)
+    }
+}
+
+impl ECDSAPublicKey {
+    pub fn as_bytes(&self) -> &[u8] {
+        return &self.0
+    }
+    pub fn to_bytes(&self) -> [u8;32] {
+        return self.0
+    }
+    pub fn to_usable_type(&self) -> Result<VerifyingKey,ecdsa::Error> {
+        let key: ecdsa::VerifyingKey<Secp256k1> = VerifyingKey::from_sec1_bytes(&self.0)?;
+        return Ok(key)
+    }
+    pub fn verify<T: AsRef<[u8]>>(&self, msg: T, signature: ECDSASignature) {
+        
     }
 }
 
